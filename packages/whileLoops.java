408d72f9eb2c2e16a819030160cb5d962eee5127
ALI <ahmedaliou94@gmail.com>
2021-10-01 10:05:07 -0400

    while loops

---
+++ packages/whileLoops.java
@@ -0,0 +1,35 @@
+package packages;
+
+public class whileLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Use a while loop to add up the numbers 1 to 20 into one variable.
+		// Print out the sum at the end
+
+		int i = 1;
+//		int sum = 0;
+//		
+//
+//		while (i <= 20) {
+//
+//			sum += i ;
+//			
+//			
+//			System.out.println(sum);
+//			
+//			i++;
+//
+//		}
+
+		while (i <= 20) {
+
+			if (i % 2 == 0)
+				System.out.println(i);
+              i++;
+
+		}
+
+	}
+}
